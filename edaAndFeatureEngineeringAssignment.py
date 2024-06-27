b5b51d3a63d69e6caea71780c4dc20bc6c77d4a8
harshil <harshilpatel2391@gmail.com>
2024-06-27 09:27:08 -0400

    load dataset

---
+++ edaAndFeatureEngineeringAssignment.py
@@ -0,0 +1,10 @@
+# import numpy as np
+import pandas as pd
+
+# import seaborn as sb
+# import scipy as sp
+# import matplotlib.pyplot as plt
+
+df = pd.read_csv("dataset.csv")
+# print(df.head(5))
+print(df.shape)
